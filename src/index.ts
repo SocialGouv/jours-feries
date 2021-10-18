68d1f84a5749611ce38c753346eeca2b2f926d10
pom421 <mauguet.po@gmail.com>
2021-10-18 12:59:25 +0200

    feat: add jest config for ts

--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,20 @@ const addDays = (date: Date, days: number) =>
 
 const paques = function (year: number) {
   const a = year % 19;
-  const century = Number(year / 100);
+  const century = Math.floor(year / 100);
   const yearsAfterCentury = year % 100;
   const d =
-    (19 * a +
-      century -
-      Number(century / 4) -
-      Number((Number(century - (century + 8) / 25) + 1) / 3) +
-      15) %
-    30;
-  const e =
-    (32 +
-      2 * (century % 4) +
-      2 * Number(yearsAfterCentury / 4) -
-      d -
-      (yearsAfterCentury % 4)) %
-    7;
-  const f = d + e - 7 * Number((a + 11 * d + 22 * e) / 451) + 114;
-
-  const month = Number(f / 31);
+      (19 * a +
+        century -
+        Math.floor(century / 4) -
+        Math.floor((Math.floor(century - (century + 8) / 25) + 1) / 3) +
+        15) %
+      30;
+  const e = (32 + 2 * (century % 4) + 2 * Math.floor(yearsAfterCentury / 4) - d - (yearsAfterCentury % 4)) % 7;
+  const f = d + e - 7 * Math.floor((a + 11 * d + 22 * e) / 451) + 114;
+  const month = Math.floor(f / 31);
   const day = (f % 31) + 1;
+
   return new Date(year, month - 1, day);
 };
 
@@ -47,8 +41,5 @@ const fetesAlsace = (year: number) => ({
 
 const getJoursFeries = (year: number, options?: { alsace: boolean }) =>
   options?.alsace ? { ...fetes(year), ...fetesAlsace(year) } : fetes(year);
-
-
-console.log(getJoursFeries(2018))
 
 export default getJoursFeries;
